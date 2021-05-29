6bc9a215f68cee7ebb9b87c8fc4a06bb3e842260
JobaDiniz <me@jobertodiniz.com>
2021-05-29 11:00:02 -0300

    google credentials in terraform cloud

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/vagrant/lab-devops-cloud-315211-e7d9844a1880.json")
-
   project = "lab-devops-cloud-315211"
   region  = "us-west1"
   zone    = "us-west1-b"
